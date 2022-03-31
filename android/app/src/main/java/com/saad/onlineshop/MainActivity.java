ae9785c75dd3dc020117b006bc16f03303ec7fe7
Engr Saad <raselhasandurjoy@gmail.com>
2022-03-31 14:21:52 +0600

    Update MainActivity.java

--- android/app/src/main/java/com/saad/onlineshop/MainActivity.java
+++ android/app/src/main/java/com/saad/onlineshop/MainActivity.java
@@ -1,6 +1,6 @@
 package com.saad.onlineshop;
 
-...................................................................................................................................................
+..................................................................................................................................................
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
